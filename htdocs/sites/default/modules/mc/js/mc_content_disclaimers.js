dee9c5cf9ebdbd97c2d537cb575f93491ac1f279
Samer Ali <samer@sameronline.com>
2012-04-18 08:10:22 -0400

    Removed content_disclaimer

--- htdocs/sites/default/modules/mc/js/mc_content_disclaimers.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("#disclaimer-link").colorbox({width:"720", height:"530", inline:true, href:"#disclaimer", opacity: 0.6});
-	$('#ok-button').click(function(){
-  		$.fn.colorbox.close();
-	});
-});
-
