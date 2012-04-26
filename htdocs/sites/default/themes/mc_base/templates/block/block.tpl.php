4711e13496f8a05900096023fd5f31cbe0f8d384
Samer Ali <samer@sameronline.com>
2012-04-26 08:08:41 -0400

    Block tpl title fix

--- htdocs/sites/default/themes/mc_base/templates/block/block.tpl.php
+++ htdocs/sites/default/themes/mc_base/templates/block/block.tpl.php
@@ -28,7 +28,7 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?> <?php print $context_block_classes ?>">
 	<div class="block-title">
 		<?php if ($block->subject): ?>
-		  <h2><?php print $block->subject ?></h2>
+		  <?php print $block->subject ?>
 		<?php endif;?>
 	</div>
 
