3c599a5599ccd852e637c22dca2238aae8c144d6
Samer Ali <samer@sameronline.com>
2012-04-26 08:14:03 -0400

    fixs to the boxes theme function

--- htdocs/sites/default/themes/mc_base/template.php
+++ htdocs/sites/default/themes/mc_base/template.php
@@ -274,8 +274,8 @@ function mc_base_pager($tags = array(), $limit = 10, $element = 0, $parameters =
 
 
 function mc_base_boxes_box($block) {
-  $output = "<div id='boxes-box-" . $block['delta'] . "' class='boxes-box" . (!empty($block['editing']) ? ' boxes-box-editing' : '') . "'>";
-  $output .= '<div class="boxes-box-content">' . $block['content'] . '</div>';
+  //$output = "<div id='boxes-box-" . $block['delta'] . "' class='boxes-box" . (!empty($block['editing']) ? ' boxes-box-editing' : '') . "'>";
+  $output .= $block['content'];
   if (!empty($block['controls'])) {
     $output .= '<div class="boxes-box-controls">';
     $output .= $block['controls'];
@@ -284,6 +284,6 @@ function mc_base_boxes_box($block) {
   if (!empty($block['editing'])) {
     $output .= '<div class="box-editor">' . $block['editing'] . '</div>';
   }
-  $output .= '</div>';
+  //$output .= '</div>';
   return $output;
 }
