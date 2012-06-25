2c1169b2faabec09eb5f2a4f3ab221d203f75e69
Samer Ali <samer@sameronline.com>
2012-06-25 10:39:44 -0400

    deploy sh script

--- htdocs/sites/miamitech.org/deploy.sh
+++ htdocs/sites/miamitech.org/deploy.sh
@@ -1,5 +1,4 @@
-git pull origin mt
-cd ../../../scripts
-sudo sh fix-permissions.sh ../htdocs samer
-cd ../htdocs/sites/miamitech.org
+git pull origin $1
+sudo sh /srv/www/miamitech/dev.miamitech.org/scripts/fix-permissions.sh /srv/www/miamitech/dev.miamitech.org/htdocs $2
 drush updb -y && drush fra -y
+sudo sh /srv/www/miamitech/dev.miamitech.org/scripts/fix-permissions.sh /srv/www/miamitech/dev.miamitech.org/htdocs $2
