ca90b16e2601e4e2b4b606609d49ee1848ac40bb
Samer Ali <samer@sameronline.com>
2012-06-25 13:22:34 -0400

    userflow alter

--- htdocs/sites/miamitech.org/deploy.sh
+++ htdocs/sites/miamitech.org/deploy.sh
@@ -1,4 +1,5 @@
 git pull origin $1
 sudo sh /srv/www/miamitech/dev.miamitech.org/scripts/fix-permissions.sh /srv/www/miamitech/dev.miamitech.org/htdocs $2
-drush updb -y && drush fra -y
+drush updb -y
+drush fra -y
 sudo sh /srv/www/miamitech/dev.miamitech.org/scripts/fix-permissions.sh /srv/www/miamitech/dev.miamitech.org/htdocs $2
