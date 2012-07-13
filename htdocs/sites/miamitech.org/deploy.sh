b2f2df51e5164782b64283c412da12a4fc1e76b5
Samer Ali <samer@sameronline.com>
2012-07-13 12:18:39 -0400

    New drush command: drush fras

--- htdocs/sites/miamitech.org/deploy.sh
+++ htdocs/sites/miamitech.org/deploy.sh
@@ -4,5 +4,5 @@ drush cc all
 drush updb -y
 #sudo sh /srv/www/miamitech/dev.miamitech.org/scripts/fix-permissions.sh /srv/www/miamitech/dev.miamitech.org/htdoc $2
 drush cc all
-drush fra -y
+drush fras -y
 #sudo sh /srv/www/miamitech/dev.miamitech.org/scripts/fix-permissions.sh /srv/www/miamitech/dev.miamitech.org/htdocs $2
