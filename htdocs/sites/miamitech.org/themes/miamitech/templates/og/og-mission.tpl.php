d979801483deee17f7842ed4836c98814597487b
Samer Ali <samer@sameronline.com>
2012-07-30 14:21:42 -0400

    mission statement fixs

--- htdocs/sites/miamitech.org/themes/miamitech/templates/og/og-mission.tpl.php
+++ htdocs/sites/miamitech.org/themes/miamitech/templates/og/og-mission.tpl.php
@@ -1,9 +1,7 @@
-<div id="mission" class="og-mission">
-<?php if (!empty($mission)) { ?>
-  <?php print $mission; ?>
-<?php } ?>
+  <?php if (!empty($mission)) { ?>
+    <?php print $mission; ?>
+  <?php } ?>
 
-<?php if(!empty($url)){ ?>
-	<p><?php echo $url ?></p>
-<?php } ?>
-</div>
+  <?php if(!empty($url)){ ?>
+      <p><?php echo $url ?></p>
+  <?php } ?>
