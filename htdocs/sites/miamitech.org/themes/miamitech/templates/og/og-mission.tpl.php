1452691656e194d5b318592beb82353cfaeb8fcf
Samer Ali <samer@sameronline.com>
2012-07-30 12:30:06 -0400

    Fix for mission statement themeing logic

---
+++ htdocs/sites/miamitech.org/themes/miamitech/templates/og/og-mission.tpl.php
@@ -0,0 +1,9 @@
+<div id="mission" class="og-mission">
+<?php if (!empty($mission)) { ?>
+  <?php print $mission; ?>
+<?php } ?>
+
+<?php if(!empty($url)){ ?>
+	<p><?php echo $url ?></p>
+<?php } ?>
+</div>
