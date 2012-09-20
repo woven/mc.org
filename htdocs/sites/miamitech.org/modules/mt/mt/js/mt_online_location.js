0cc1ecb5505047288632d196b57254c048bb2c72
Santiago Rios <santiago@woven.org>
2012-09-20 13:09:27 -0400

    Hides place when the online event field is checked and fixes the bug of online event field not being saved

--- htdocs/sites/miamitech.org/modules/mt/mt/js/mt_online_location.js
+++ htdocs/sites/miamitech.org/modules/mt/mt/js/mt_online_location.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-    $('#edit-locations-0-field-online-event-value').change(function(){
-        var fieldset = $(this).closest('.fieldset-content');
-        var location_wrapper = $(fieldset).find('.default-location-wrapper');
+    $('#edit-field-online-event-value').change(function(){
+        var location_wrapper = $('#edit-field-place-0-nid-nid-wrapper').closest('.default-location-wrapper');
         $(location_wrapper).slideToggle();
         $(location_wrapper).find('input').each(function(){
             $(this).val('');
