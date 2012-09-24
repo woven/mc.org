e52df5bbc758f77235e93b2ed70f75f5ef139807
Samer Ali <samer@sameronline.com>
2012-09-24 13:42:47 -0400

    clean up for mt code base

--- htdocs/sites/miamitech.org/modules/mt/features/mt_feedbackify/feedbackify.js
+++
@@ -1,8 +0,0 @@
-Drupal.behaviors.feedbackifylink = function (context) {
-	if(Drupal.settings.feedbackify.feedbackify_id){
-		$("a[rel*='feedbackify']").click(function (e) {
-            FBY.showForm(Drupal.settings.feedbackify.feedbackify_id);
-            e.preventDefault();
-        });
-	}
-};
