cdc058651b9c54c1b8b8d96b4c233f36efe03cad
Samer Ali <samer@sameronline.com>
2012-10-01 16:26:06 -0400

    choose a place

--- htdocs/sites/all/modules/mycommunity/mc/features/ft_event/js/mc_online_location.js
+++ htdocs/sites/all/modules/mycommunity/mc/features/ft_event/js/mc_online_location.js
@@ -7,3 +7,30 @@ $(document).ready(function() {
         });
     })
 });
+
+Drupal.behaviors.EventPlaceSelect = function(context){
+    if(context == document){
+        $choice = $('input:radio[name=field_place_choice[value]]');
+        $choice.find(":first-child").hide();
+        $choice.change(
+            function(e){
+                $this= $(this);
+                cls = "input:radio[name=field_place_choice[value]]:checked";
+                $selected = $(cls);
+                switch($selected.val()){
+                    case 'nr':
+                        $(".group-place .location").hide();
+                        $(".group-place #edit-field-place-0-nid-nid-wrapper").show();
+                    break;
+                    case 'new':
+                        $(".group-place .location").show();
+                        $(".group-place #edit-field-place-0-nid-nid-wrapper").hide();
+                    break;
+                }
+            }
+        );
+
+        $choice.change();
+
+    }
+}
