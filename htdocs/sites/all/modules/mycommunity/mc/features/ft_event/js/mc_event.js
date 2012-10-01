f409ed959d570a9ad1d6b8a1a715c86d8fbf0f93
Samer Ali <samer@sameronline.com>
2012-10-01 17:30:57 -0400

    various fixs for ft_event

--- htdocs/sites/all/modules/mycommunity/mc/features/ft_event/js/mc_event.js
+++ htdocs/sites/all/modules/mycommunity/mc/features/ft_event/js/mc_event.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
-    $('#edit-field-online-event-value').change(function(){
-        var location_wrapper = $('#edit-field-place-0-nid-nid-wrapper').closest('.default-location-wrapper');
-        $(location_wrapper).slideToggle();
-        $(location_wrapper).find('input').each(function(){
-            $(this).val('');
-        });
-    })
-});
+Drupal.behaviors.EventOnlineEvent = function(context){
+    if(context == document){
+        $('#edit-field-online-event-value').change(function(){
+            var location_wrapper = $('#edit-field-place-0-nid-nid-wrapper').closest('.default-location-wrapper');
+            $(location_wrapper).slideToggle();
+            $(location_wrapper).find('input').each(function(){
+                $(this).val('');
+            });
+        })
+    }
+}
 
 Drupal.behaviors.EventPlaceSelect = function(context){
     if(context == document){
