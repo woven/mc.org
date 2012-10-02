815ebcab30406197765e4519531083223b85d14b
Samer Ali <samer@sameronline.com>
2012-10-02 13:59:52 -0400

    improve online event checkbox logic

--- htdocs/sites/all/modules/mycommunity/mc/features/ft_event/js/mc_event.js
+++ htdocs/sites/all/modules/mycommunity/mc/features/ft_event/js/mc_event.js
@@ -1,12 +1,9 @@
 Drupal.behaviors.EventOnlineEvent = function(context){
     if(context == document){
         $('#edit-field-online-event-value').change(function(){
-            var location_wrapper = $('#edit-field-place-0-nid-nid-wrapper').closest('.default-location-wrapper');
-            $(location_wrapper).slideToggle();
-            $(location_wrapper).find('input').each(function(){
-                $(this).val('');
-            });
-        })
+            $this = $(this);
+            $('.group-place').slideToggle();
+        });
     }
 }
 
