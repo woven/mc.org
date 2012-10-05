4e1bff7a60132e434c13098667b51ad674708ea8
Samer <samer@woven.org>
2012-10-05 09:04:41 -0400

    db-fetch script

--- htdocs/sites/miamitech.org/db-fetch.sh
+++ htdocs/sites/miamitech.org/db-fetch.sh
@@ -18,6 +18,6 @@ drush sql-cli < db.sql
 rm -f db.sql.gz
 rm -f db.sql
 #do some drupal clean up and cache cleaning
-#drush cc all
-#drush updb -y
-#drush cc all
+drush cc all
+drush updb -y
+drush cc all
