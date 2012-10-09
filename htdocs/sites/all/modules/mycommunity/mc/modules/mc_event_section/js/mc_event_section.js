9f95178181a3fc4d60d8b09d3760dbc83ba849ff
Samer Ali <samer@sameronline.com>
2012-10-09 18:12:21 -0400

    mc_event_section duplicates logic

--- htdocs/sites/all/modules/mycommunity/mc/modules/mc_event_section/js/mc_event_section.js
+++ htdocs/sites/all/modules/mycommunity/mc/modules/mc_event_section/js/mc_event_section.js
@@ -1,23 +1,15 @@
 $(document).ready(function() {
-  var views = $('.view-mc-events-by-dates');
-  if($(views).length){
-    $(views).each(function(){
-      var view = $(this);
-      var duplicates = $(view).find('.duplicates');
-      if($(duplicates).length){
-        $(duplicates).each(function(){
-          var duplicate = $(this);
-          var events = $(duplicate).find('.event');
-          if($(events).length>1){
+  $('.event-section .duplicates').each(function(){
+        var $duplicate = $(this);
+        var events = $duplicate.find('.event');
+        if($(events).length>1){
+            console.log("test");
             var count = $(events).length - 1;
-            var firstEvent = events.closest('.duplicates').find('.first');
+            var firstEvent = $duplicate.find('.first');
             var showMoreDiv = '<div class="show-more-events"><a href="#">Show ' + count + ' similar</a></div>';
             firstEvent.append(showMoreDiv);
-          }
-        });
-      }
-    }); 
-  }
+        }
+  });
 
   $('.show-more-events a').click(function(event){
     var duplicates = $(this).closest('.duplicates');
