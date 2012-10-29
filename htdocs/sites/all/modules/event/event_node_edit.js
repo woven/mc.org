57e72de6d63a4dece87b89e7b53b3a0e3d2af8f9
Samer Ali <samer@sameronline.com>
2012-10-29 18:32:58 -0400

    DatePicker bug with checkbox 'event has time'

--- htdocs/sites/all/modules/event/event_node_edit.js
+++ htdocs/sites/all/modules/event/event_node_edit.js
@@ -1,20 +1,28 @@
-if (Drupal.jsEnabled) {
-  // Define the selectors of the fields that needs hiding/showing
-  var times = new Array("#edit-event-start-exploded-hour", "#edit-event-start-exploded-minute", "#edit-event-end-exploded-hour", "#edit-event-end-exploded-minute", "#edit-event-start-exploded-ampm", "#edit-event-end-exploded-ampm");
-  var end_date = new Array("#edit-event-end-exploded-wrapper");
-  $(document).ready(function () {
-    // Show/hide those fields after page load
-    event_switch(times, $("#edit-event-has-time").attr("checked") ? 'show' : 'hide');
-    event_switch(end_date, $("#edit-event-has-end-date").attr("checked") ? 'show' : 'hide');
-   
-    // Attaching action for the "click" event
-    $("#edit-event-has-time").click(function () {
-      event_switch(times, 'toggle');
-    });
-    $("#edit-event-has-end-date").click(function () {
-      event_switch(end_date, 'toggle');
-    });
-  });
+Drupal.behaviors.toogle = function (context) {
+      // Define the selectors of the fields that needs hiding/showing
+      var times = new Array("#edit-event-start-exploded-hour", "#edit-event-start-exploded-minute", "#edit-event-end-exploded-hour", "#edit-event-end-exploded-minute", "#edit-event-start-exploded-ampm", "#edit-event-end-exploded-ampm");
+      var end_date = new Array("#edit-event-end-exploded-wrapper");
+
+      // Show/hide those fields after page load
+      event_switch(times, $("#edit-event-has-time").attr("checked") ? 'show' : 'hide');
+      event_switch(end_date, $("#edit-event-has-end-date").attr("checked") ? 'show' : 'hide');
+
+      // Attaching action for the "click" event
+      $("#edit-event-has-time").click(function () {
+          if(this.checked){
+              event_switch(times, 'show');
+          }else{
+              event_switch(times, 'hide');
+          }
+      });
+
+      $("#edit-event-has-end-date").click(function () {
+          if(this.checked){
+              event_switch(end_date, 'show');
+          }else{
+              event_switch(end_date, 'hide');
+          }
+      });
 }
 
 /**
