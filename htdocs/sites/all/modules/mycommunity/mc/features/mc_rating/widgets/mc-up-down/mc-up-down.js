a3a3fba9b44c2f703d03e8106e9c24d5d3324842
Samer Ali <samer@sameronline.com>
2012-12-04 12:35:16 -0500

    update css to reflect both logged in and non logged in users

--- htdocs/sites/all/modules/mycommunity/mc/features/mc_rating/widgets/mc-up-down/mc-up-down.js
+++ htdocs/sites/all/modules/mycommunity/mc/features/mc_rating/widgets/mc-up-down/mc-up-down.js
@@ -3,9 +3,9 @@ Drupal.behaviors.rate_mc_up_down = function (context) {
   var widget_cls = ".rate-widget-mc_up_down";
   $(widget_cls + " a.rate-btn", context).not(".btn-active").each(function(index) {
     $(this).click(function(e){
+      e.preventDefault();
       $(this).parent(widget_cls).find("a.btn-active").removeClass("btn-active");
       $(this).addClass('.btn-active');
-      console.log("test");
     });
   });
 };
