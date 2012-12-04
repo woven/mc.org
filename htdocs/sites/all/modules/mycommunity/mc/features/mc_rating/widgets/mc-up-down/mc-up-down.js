7c32f4e2b797b607d2342865aa484896a0155f32
Samer Ali <samer@sameronline.com>
2012-12-04 12:46:37 -0500

    instant rating buttons

--- htdocs/sites/all/modules/mycommunity/mc/features/mc_rating/widgets/mc-up-down/mc-up-down.js
+++ htdocs/sites/all/modules/mycommunity/mc/features/mc_rating/widgets/mc-up-down/mc-up-down.js
@@ -1,11 +1,17 @@
-//"rate-widget-mc_up_down";
+/*
+Instant Rating for MC UP down widget
+ */
 Drupal.behaviors.rate_mc_up_down = function (context) {
   var widget_cls = ".rate-widget-mc_up_down";
-  $(widget_cls + " a.rate-btn", context).not(".btn-active").each(function(index) {
-    $(this).click(function(e){
-      e.preventDefault();
-      $(this).parent(widget_cls).find("a.btn-active").removeClass("btn-active");
-      $(this).addClass('.btn-active');
-    });
+
+  //when clicking on non active href
+  $(widget_cls + " a.rate-btn", context).not("a.btn-active").click(function(){
+    $(this).parents('.rate-widget-mc_up_down').find("a.btn-active").removeClass("btn-active");
+    $(this).addClass('btn-active');
+  });
+
+  //when clicking on active hrefs
+  $(widget_cls + " a.rate-btn.btn-active", context).click(function(){
+    $(this).removeClass('btn-active');
   });
 };
